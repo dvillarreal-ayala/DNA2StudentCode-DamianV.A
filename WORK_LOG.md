49419b6e94a9c9b4d7db9e6bce2b8b8fea140af7
damianayala <damian.villarreal-ayala@menloschool.org>
2024-10-11 12:05:20 -0700

    oct 11 commit; prep work mostly done

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date   |  Time   |                                                                                                                                                                               Update |
+|:-------|:-------:|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Oct 9  | 12-1pm  | Thinking about marking instances of the first letter of the STR as I iterate through the sequence, and comparing how many numbers are between the indices and the length of the STR. |
+| OCT 11 | 10-11pm |                                                          Added comments explaining my thought process regarding the for loop and work needed for DNA in the code and in my notebook. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/DNA.java
+++ src/DNA.java
@@ -6,7 +6,7 @@
  * at Menlo School in Atherton, CA
  *</p>
  * <p>
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Damian Villarreal-Ayala
  *</p>
  */
 
@@ -16,7 +16,32 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+        // Used to track the longest consecutive run of STR
+        int numOfConsecutive = 0;
 
+        /*
+          In this for loop, iterate through the sequence and search for instances of the STR
+          Iterate by char, comparing char to first char of STR.
+          Idea for checking if the selected section matches the STR is by turning STR & selected
+          String section/chars into a unique # so that you can do faster comparisons.
+          ideas/concerns:
+          - BECAUSE there are only four letters (ACTG), we don't have to worry too much about the
+          ASCII values adding together in a numerical order different from the order of the STR.
+          WHat COULD be done? Create a separate checker that would take in the STR.length() chars
+          starting at i, then compares the chars to the STR in the intended order. If the chars don't
+          add up to the same value as the STR's unique number or fails a comparison within the helper, then
+          reject.
+         */
+        /**
+         * ASCII value for CAPITAL A is 65, Z is 90
+         */
+        for(int i = 0; i < 3; i++)
+        {
+            char monitored = sequence.charAt(i);
+
+        }
+
+        // Return 0 when there is no STR throughout the sequence
         return 0;
     }
 }
